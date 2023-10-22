5a473a85158fed1f6a9bfc40f7c7c220e6640f3c
Mirzalazuardi <mirzalazuardi@gmail.com>
2023-10-23 00:37:45 +0700

    nvim_setup 23oct2023

---
+++ lua/plugins.lua
@@ -0,0 +1,69 @@
+require ('packer').startup(function(use)
+	-- packer can manage itself
+	-- :PackerSync
+	use 'wbthomason/packer.nvim'
+
+	-- Comment
+	-- gcc gbc gcw gc$ gc2j gc4k
+	use {
+	    'numToStr/Comment.nvim',
+	    config = function()
+		require('Comment').setup()
+	    end
+	}
+
+	-- mason
+	-- :MasonUpdate
+	-- :MasonInstall ruby-lsp
+	use {
+		"williamboman/mason.nvim",
+		run = ":MasonUpdate" -- :MasonUpdate updates registry
+	}
+
+	-- alpha-nvim
+	use {
+		'goolord/alpha-nvim',
+		requires = {'nvim-tree/nvim-web-devicons'},
+		config = function ()
+			require'alpha'.setup(require'alpha.themes.startify'.config)
+		end
+	}
+	
+	-- nvim-cmp - completions engine
+	use "hrsh7th/cmp-nvim-lsp"
+	use "hrsh7th/cmp-buffer"
+	use 'hrsh7th/cmp-path'
+	use 'hrsh7th/cmp-cmdline'
+	use 'hrsh7th/nvim-cmp'
+
+	-- git-signs
+  use{
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("configs.gitsigns")
+        end,
+  }
+	-- Lualine
+	use {
+		'nvim-lualine/lualine.nvim',
+		requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+	}
+	
+	-- nvim-visual-multi
+	-- <C-n> n N q Q
+	use 'mg979/vim-visual-multi'
+
+	-- nvim-tree.lua
+	use 'nvim-tree/nvim-tree.lua'
+
+	-- telescope
+	-- <leader>ff <leader>fg <leader>fb <leader>fh <leader>fs <leader>fc
+	use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.4',
+	-- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	-- FZF
+	use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+end)
