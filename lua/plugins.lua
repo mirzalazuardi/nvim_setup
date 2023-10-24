459860816001ae0c3e1ad18d76b99df84c0a76d7
Mirzalazuardi <mirzalazuardi@gmail.com>
2023-10-24 12:04:47 +0700

    add surround

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,6 +3,17 @@ require ('packer').startup(function(use)
 	-- :PackerSync
 	use 'wbthomason/packer.nvim'
 
+  -- surround
+  use {
+    "kylechui/nvim-surround",
+    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  }
+
 	-- Comment
 	-- gcc gbc gcw gc$ gc2j gc4k
 	use {
