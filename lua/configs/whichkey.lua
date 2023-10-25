304afcfeaa6fbd8b203e9798d85d0a60d6b26000
Mirzalazuardi <mirzalazuardi@gmail.com>
2023-10-25 14:27:41 +0700

    add key for open tree file

--- lua/configs/whichkey.lua
+++ lua/configs/whichkey.lua
@@ -8,5 +8,6 @@ wk.register({
   ["<leader>fs"] = { "<cmd>Telescope git_status<cr>", "Tele Git status" },
   ["<leader>fc"] = { "<cmd>Telescope git_commits<cr>", "Tele Git commits" },
   ["<leader>fr"] = { "<cmd>Telescope oldfiles<cr>", "Tele Open Recent File" },
+  ["<leader>fe"] = { "<cmd>NvimTreeFindFileToggle<cr>", "Nvim tree explorer" },
   ["<leader>fn"] = { "<cmd>enew<cr>", "New File" },
 })
